4b368b7aa7add875a8b29182fb690b74df71e055
Ben othman Rayen <raydineri07@gmail.com>
2025-11-27 09:21:13 +0100

    creating project and intializ

---
+++ src/main/java/com/iteam/iteam_api/IteamApiApplication.java
@@ -0,0 +1,13 @@
+package com.iteam.iteam_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iteam/iteam_api/IteamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteam.iteam_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
